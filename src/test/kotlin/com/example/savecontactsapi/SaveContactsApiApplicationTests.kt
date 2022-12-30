169dd7401798bdf94bf7c6ff5d2b4a9a2af7b6e6
Gabriel Nascimento <gabriel.rocha1301@gmail.com>
2022-12-29 23:13:41 -0300

    Revert "Revert "Commit: entidade""
    This reverts commit 9c93441fc85486e5443228d52a237414de8c865f.

---
+++ src/test/kotlin/com/example/savecontactsapi/SaveContactsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.savecontactsapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SaveContactsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
