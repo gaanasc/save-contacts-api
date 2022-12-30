61cfab1174bce010488503c12eb066321ef136cc
Gabriel Nascimento <gabriel.rocha1301@gmail.com>
2022-12-29 23:16:27 -0300

    Revert "Revert "feat: ContactsRepository""
    This reverts commit 0e658594fdb22ba4accdd708473a924f1b0074a8.

---
+++ src/main/kotlin/com/example/savecontactsapi/repository/ContactsRepository.kt
@@ -0,0 +1,9 @@
+package com.example.savecontactsapi.repository
+
+import com.example.savecontactsapi.entity.Contacts
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ContactsRepository : JpaRepository<Contacts, Long> {
+}
