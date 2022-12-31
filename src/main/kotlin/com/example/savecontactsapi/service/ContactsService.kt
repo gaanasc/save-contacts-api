2db39c800aaacc1f1c2c11372fb59b49613084a3
Gabriel Nascimento <gabriel.rocha1301@gmail.com>
2022-12-30 23:24:44 -0300

    feat: regras de negócios mapeadas na camada de serviço

--- src/main/kotlin/com/example/savecontactsapi/service/ContactsService.kt
+++ src/main/kotlin/com/example/savecontactsapi/service/ContactsService.kt
@@ -24,4 +24,45 @@ class ContactsService(
         }
         return idContacts
     }
+
+    fun updateContacts(id: Long, newContacts: Contacts): Contacts {
+        val contacts = repository.findById(id).orElseThrow {
+            SaveContactsNotFoundException(
+                "Ja existe um contato com o id $id, ou o mesmo não existe"
+            )
+        }
+
+        contacts.apply {
+            this.condominio = newContacts.condominio
+            this.email = newContacts.email
+            this.endereco = newContacts.endereco
+            this.bairro = newContacts.bairro
+            this.complemento = newContacts.complemento
+            this.cep = newContacts.cep
+            this.cidade = newContacts.cidade
+            this.nome_sindico = newContacts.nome_sindico
+            this.tel_sindico = newContacts.tel_sindico
+            this.cel_sindico = newContacts.cel_sindico
+            this.nome_zelador = newContacts.nome_zelador
+            this.tel_zelador = newContacts.tel_zelador
+            this.cel_zelador = newContacts.cel_zelador
+            this.observacao = newContacts.observacao
+        }
+        return repository.save(newContacts)
+    }
+
+    fun saveContacts(contacts: Contacts): Contacts{
+        return repository.save(contacts)
+    }
+
+    fun deleteContacts(id: Long) {
+        val contacts = repository.findById(id).orElseThrow {
+            SaveContactsNotFoundException(
+                "O id $id não existe"
+            )
+        }
+        repository.delete(contacts)
+    }
 }
+//TODO: verificar duplicidade de nome de
+// condominio, regra de cadastrar uma nova ou não
