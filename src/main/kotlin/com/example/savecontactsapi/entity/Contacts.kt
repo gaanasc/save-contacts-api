e25edcb68f1b482adc8354f7a42555a2a11b8049
Gabriel Nascimento <gabriel.rocha1301@gmail.com>
2022-12-30 23:19:38 -0300

    feat: adicionado novos itens na entidade

--- src/main/kotlin/com/example/savecontactsapi/entity/Contacts.kt
+++ src/main/kotlin/com/example/savecontactsapi/entity/Contacts.kt
@@ -19,15 +19,36 @@ data class Contacts(
     @Column(nullable = false, length = 100)
     var endereco: String,
 
+    @Column(nullable = false, length = 30)
+    var bairro: String,
+
+    @Column(nullable = false, length = 20)
+    var complemento: String,
+
+    @Column(nullable = false, length = 8)
+    var cep: Int,
+
+    @Column(nullable = false, length = 15)
+    var cidade: String,
+
+    @Column(nullable = false, length = 20)
+    var nome_sindico: String,
+
     @Column(nullable = false, length = 15)
     var tel_sindico: Long,
 
     @Column(nullable = true, length = 15)
     var cel_sindico: Long,
 
+    @Column(nullable = false, length = 20)
+    var nome_zelador: String,
+
     @Column(nullable = false, length = 15)
     var tel_zelador: Long,
 
     @Column(nullable = true, length = 15)
-    var cel_zelador: Long
+    var cel_zelador: Long,
+
+    @Column(nullable = true, length = 250)
+    var observacao: String
 )
